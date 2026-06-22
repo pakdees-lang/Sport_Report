5ff8ecb267c94845c0edb903b956527bc2c826cf
pakdees-lang <pakdee.s@prakklang.ac.th>
2026-06-22 21:17:41 +0700

    Add files via upload
    Update Sport_Report V.4

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "sport-rank-report-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./service-worker.js", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "sport-rank-report-v4";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./service-worker.js", "./icon-192.png", "./icon-512.png", "./team-template.xlsx"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
