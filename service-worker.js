aa1dc2b668ecd19d2a4d1d8bec8409918cb2b103
pakdees-lang <pakdee.s@prakklang.ac.th>
2026-07-12 12:30:11 +0700

    Add files via upload
    sport-report-mobile-app-v6-trial

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sport-report-v5";
+const CACHE_NAME = "sport-report-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./service-worker.js", "./icon-192.png", "./icon-512.png", "./team-template.xlsx"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
